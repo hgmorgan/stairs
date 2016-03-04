d313fce90b68b6ed9c043722467b39008e5492e7
hgmorgan <hgmorgan2012@gmail.com>
2016-03-04 13:20:11 -0500

    initial commit

---
+++ src/stairs/StairCase.java
@@ -0,0 +1,37 @@
+package stairs;
+
+public class StairCase {
+
+    public static void main(String[] args) {
+        printStairs();
+
+            }
+
+            public static void printStairs() {
+                for (int allStairs=1; allStairs<=1; allStairs++) {
+                    
+                    for (int stair = 1; stair <= 1; stair++) {
+                        System.out.println("*          *");
+                    } 
+                    
+                    for (int stair = 1; stair <= 1; stair++) {
+                        System.out.println("**        **");
+                    }    
+                    
+                    for (int stair = 1; stair <= 1; stair++) {
+                        System.out.println("***      ***");
+
+                    }
+                    
+                    for (int stair = 1; stair <= 1; stair++) {
+                        System.out.println("****    ****");
+
+                    }
+                  
+                    for (int stair = 1; stair <= 1; stair++) {
+                        System.out.println("*****  *****");
+
+                    }
+                }
+            }
+        }
